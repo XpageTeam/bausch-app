7c220ef7a79854d3aa93e2b78588aedeea3f36f7
bonsoirelliot <privetdek16@gmail.com>
2021-09-30 12:28:08 +0500

    добавил стили, создаю новую ветку

---
+++ lib/theme/styles.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const h2 = TextStyle(
+    color: Color(0xFF2D2D2D),
+    fontWeight: FontWeight.w500,
+    fontSize: 21,
+    height: 25 / 21,
+  );
+
+  static const h3 = TextStyle(
+    color: Color(0xFF2D2D2D),
+    fontWeight: FontWeight.w500,
+    fontSize: 17,
+    height: 20 / 17,
+  );
+
+  static const h4 = TextStyle(
+    color: Color(0xFF2D2D2D),
+    fontWeight: FontWeight.normal,
+    fontSize: 17,
+    height: 20 / 17,
+  );
+
+  static const p1 = TextStyle(
+    color: Color(0xFF2D2D2D),
+    fontWeight: FontWeight.normal,
+    fontSize: 14,
+    height: 20 / 14,
+  );
+
+  static const p2 = TextStyle(
+    color: Color(0xFF2D2D2D),
+    fontWeight: FontWeight.normal,
+    fontSize: 12,
+    height: 14 / 12,
+  );
+}
