61123715fad4585a18126a67c30fd8a1f5ad6325
bonsoirelliot <privetdek16@gmail.com>
2021-10-01 17:26:30 +0500

    что-то сделал со слайдером

--- lib/sections/home/sections/may_be_interesting_section.dart
+++ lib/sections/home/sections/may_be_interesting_section.dart
@@ -1,3 +1,6 @@
+import 'package:bausch/custom_slideshow.dart';
+import 'package:bausch/models/catalog_item_model.dart';
+import 'package:bausch/theme/app_theme.dart';
 import 'package:bausch/widgets/catalog_item/catalog_item.dart';
 import 'package:bausch/static/static_data.dart';
 import 'package:bausch/test/models.dart';
@@ -19,22 +22,27 @@ class MayBeInteresting extends StatelessWidget {
         SizedBox(
           height: 20,
         ),
-        SingleChildScrollView(
-          clipBehavior: Clip.none,
-          physics: const BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          child: IntrinsicHeight(
-            child: Row(
-              children: [
-                CatalogItem(
-                  model: Models.items[0],
-                ),
-                CatalogItem(model: Models.items[1]),
-                CatalogItem(model: Models.items[2])
-              ],
-            ),
-          ),
-        )
+        CustomImageSlideshow(
+          height: MediaQuery.of(context).size.height / 2.5,
+          indicatorBackgroundColor: Colors.white,
+          indicatorColor: AppTheme.turquoiseBlue,
+          children: List.generate(
+              (Models.items.length % 2) == 0
+                  ? Models.items.length ~/ 2
+                  : Models.items.length ~/ 2 + 1,
+              (i) => Row(
+                    children: [
+                      CatalogItem(
+                        model: Models.items[i * 2],
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      if (Models.items.asMap().containsKey(i * 2 + 1))
+                        CatalogItem(model: Models.items[i * 2 + 1])
+                    ],
+                  )),
+        ),
       ],
     );
   }
