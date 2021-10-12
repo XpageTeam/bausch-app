971c9380785febac65e01539145ff1a7c0c35f33
bonsoirelliot <privetdek16@gmail.com>
2021-10-12 13:50:47 +0500

    loading screen

--- lib/sections/loading/loading_screen.dart
+++ lib/sections/loading/loading_screen.dart
@@ -1,6 +1,11 @@
+import 'package:bausch/sections/loading/animated_content.dart';
+import 'package:bausch/static/static_data.dart';
 import 'package:bausch/theme/app_theme.dart';
+import 'package:bausch/theme/styles.dart';
+import 'package:bausch/widgets/buttons/blue_button_with_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -11,81 +16,65 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen>
     with TickerProviderStateMixin {
+  late Animation positionAnimation;
   late AnimationController controller;
-  late Animation opacityAnimation;
-  late Animation logoPositionAnimation;
+
+  //* Анимация начнется примерно через 2 секунды после initState
+  Interval interval = const Interval(0.7, 1.0, curve: Curves.easeInOut);
+
+  double opacity = 0;
 
   @override
   void initState() {
     super.initState();
-
     controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 5));
-    opacityAnimation = Tween(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: Interval(0.0, 0.5),
-      ),
-    );
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+    positionAnimation = Tween<double>(begin: 250.0, end: 0.0)
+        .animate(CurvedAnimation(parent: controller, curve: interval));
 
-    logoPositionAnimation = Tween(begin: 0.0, end: 50.0).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: Interval(0.0, 0.5),
-      ),
-    );
+    controller.addListener(() {
+      setState(() {
+        opacity = 1.0;
+      });
+    });
 
     controller.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.turquoiseBlue,
-      body: AnimatedBuilder(
-        animation: controller,
-        builder: _buildAnimation,
-      ),
-    );
-  }
-
-  Widget _buildAnimation(BuildContext context, Widget? child) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-          height: logoPositionAnimation.value as double,
-        ),
-        Opacity(
-          opacity: opacityAnimation.value as double,
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/logo_white.png',
-              ),
-              const Text(
-                'Friends',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 24,
-                  height: 31 / 24,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            //* Анимация, запускается при инициализации экрана
+            const RiveAnimation.asset(
+              'assets/loading.riv',
+              fit: BoxFit.cover,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                AnimatedOpacity(
+                  duration: const Duration(seconds: 3),
+                  opacity: opacity,
+                  curve: interval,
+                  child: AnimatedContent(
+                    animation: positionAnimation as Animation<double>,
+                  ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
-        const SizedBox(
-          height: 26,
-        ),
-        Opacity(
-          opacity: opacityAnimation.value as double,
-          child: CircleAvatar(
-            radius: 86,
-            backgroundColor: Colors.white,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
