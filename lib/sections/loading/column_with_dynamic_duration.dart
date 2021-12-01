6509dfe3957d591d65d9e7d05803ffb803bc7fb8
Danil Khandys <dreadlord74@yandex.ru>
2021-12-01 15:03:02 +0500

    123

--- lib/sections/loading/column_with_dynamic_duration.dart
+++ lib/sections/loading/column_with_dynamic_duration.dart
@@ -11,50 +11,52 @@ class ColumnWithDynamicDuration extends StatelessWidget {
   Widget build(BuildContext context) {
     final spaceBetween = (MediaQuery.of(context).size.width - 114 * 2) / 3;
 
-    return ListView.separated(
-      itemBuilder: (context, i) {
-        if (i == 1) {
-          return DelayedAnimatedTranslateOpacity(
-            offsetY: 20,
-            delay: Duration(milliseconds: 200 + i * 400),
-            child: WidgetConveyor(
-              children: [
-                SizedBox(
-                  width: spaceBetween,
-                ),
-                IntrinsicWidth(child: children[1]),
-                SizedBox(
-                  width: spaceBetween,
-                ),
-                IntrinsicWidth(child: children[2]),
-                SizedBox(
-                  width: spaceBetween,
-                ),
-                IntrinsicWidth(child: children[3]),
-                SizedBox(
-                  width: spaceBetween,
-                ),
-                IntrinsicWidth(child: children[1]),
-                SizedBox(
-                  width: spaceBetween,
-                ),
-              ],
-            ),
+    return IgnorePointer(
+      child: ListView.separated(
+        itemBuilder: (context, i) {
+          if (i == 1) {
+            return DelayedAnimatedTranslateOpacity(
+              offsetY: 20,
+              delay: Duration(milliseconds: 200 + i * 400),
+              child: WidgetConveyor(
+                children: [
+                  SizedBox(
+                    width: spaceBetween,
+                  ),
+                  IntrinsicWidth(child: children[1]),
+                  SizedBox(
+                    width: spaceBetween,
+                  ),
+                  IntrinsicWidth(child: children[2]),
+                  SizedBox(
+                    width: spaceBetween,
+                  ),
+                  IntrinsicWidth(child: children[3]),
+                  SizedBox(
+                    width: spaceBetween,
+                  ),
+                  IntrinsicWidth(child: children[1]),
+                  SizedBox(
+                    width: spaceBetween,
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return DelayedAnimatedTranslateOpacity(
+              offsetY: 20,
+              child: children[i],
+              delay: Duration(milliseconds: 200 + i * 400),
+            );
+          }
+        },
+        separatorBuilder: (context, i) {
+          return const SizedBox(
+            height: 63,
           );
-        } else {
-          return DelayedAnimatedTranslateOpacity(
-            offsetY: 20,
-            child: children[i],
-            delay: Duration(milliseconds: 200 + i * 400),
-          );
-        }
-      },
-      separatorBuilder: (context, i) {
-        return const SizedBox(
-          height: 63,
-        );
-      },
-      itemCount: children.length,
+        },
+        itemCount: children.length,
+      ),
     );
   }
 }
