4b1f63bea0f4bc8537fc2dce45657443dd7b303d
Danil Khandys <dreadlord74@yandex.ru>
2021-12-18 18:08:30 +0500

    убрал лишний импорт

--- lib/widgets/catalog_item/catalog_item.dart
+++ lib/widgets/catalog_item/catalog_item.dart
@@ -1,6 +1,5 @@
 import 'package:bausch/models/catalog_item/catalog_item_model.dart';
 import 'package:bausch/models/catalog_item/webinar_item_model.dart';
-import 'package:bausch/sections/sheets/screens/webinars/final_webinar.dart';
 import 'package:bausch/static/static_data.dart';
 import 'package:bausch/theme/styles.dart';
 import 'package:bausch/widgets/buttons/button_with_points.dart';
