19fe45bba6657e7d8f0f6bfad32f5adff22dbe5a
Danil Khandys <dreadlord74@yandex.ru>
2022-01-10 15:48:21 +0500

    прокачал модель

--- lib/models/dadata/dadata_response_data_model.dart
+++ lib/models/dadata/dadata_response_data_model.dart
@@ -2,11 +2,13 @@ class DadataResponseDataModel {
   final String street;
   final String house;
   final String? block;
+  final String? city;
   //final String streetType;
 
   DadataResponseDataModel({
     required this.street,
     required this.house,
+    this.city,
     this.block,
     //required this.streetType,
   });
@@ -15,6 +17,7 @@ class DadataResponseDataModel {
       street: (map['street_with_type'] ?? '') as String,
       house: (map['house'] ?? '') as String,
       block: map['block'] as String?,
+      city: map['city'] as String?,
       //streetType: (map['street_type'] ?? '') as String,
     );
   }
