05c9650226793ff3b0ee54ec17e55eb64a1de29d
Danil Khandys <dreadlord74@yandex.ru>
2022-01-18 15:10:15 +0500

    переделал на стетлес виджет

--- lib/sections/faq/contact_support/contact_support_screen.dart
+++ lib/sections/faq/contact_support/contact_support_screen.dart
@@ -3,7 +3,6 @@ import 'package:bausch/models/faq/topic_model.dart';
 import 'package:bausch/sections/faq/contact_support/forms_listener.dart';
 import 'package:bausch/sections/faq/contact_support/forms_provider.dart';
 import 'package:bausch/sections/faq/contact_support/forms_screen.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ContactSupportScreenArguments {
@@ -18,7 +17,7 @@ class ContactSupportScreenArguments {
   });
 }
 
-class ContactSupportScreen extends StatefulWidget
+class ContactSupportScreen extends StatelessWidget
     implements ContactSupportScreenArguments {
   final ScrollController controller;
   @override
@@ -37,18 +36,15 @@ class ContactSupportScreen extends StatefulWidget
   }) : super(key: key);
 
   @override
-  State<ContactSupportScreen> createState() => _ContactSupportScreenState();
-}
-
-class _ContactSupportScreenState extends State<ContactSupportScreen> {
-  @override
+  
   Widget build(BuildContext context) {
     return FormsProvider(
       child: FormsListener(
         child: FormsScreen(
-          controller: widget.controller,
-          topic: widget.topic,
-          question: widget.question,
+          controller: controller,
+          topic: topic,
+          question: question,
+          // fields: fields,
         ),
       ),
     );
