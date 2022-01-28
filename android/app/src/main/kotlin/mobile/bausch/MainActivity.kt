daecd51795bebd7d4e50b6fe9506c2f7377b22fa
Danil Khandys <dreadlord74@yandex.ru>
2022-01-28 13:48:34 +0500

    пуши для андроида

--- android/app/src/main/kotlin/mobile/bausch/MainActivity.kt
+++ android/app/src/main/kotlin/mobile/bausch/MainActivity.kt
@@ -1,10 +1,14 @@
 package mobile.bausch
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.annotation.NonNull
+import cloud.mindbox.mindbox_android.MindboxAndroidPlugin
+import cloud.mindbox.mobile_sdk.Mindbox
+import com.yandex.mapkit.MapKitFactory
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
-import com.yandex.mapkit.MapKitFactory
+//import cloud.mindbox.mobile_sdk
 
 
 class MainActivity: FlutterActivity() {
@@ -12,5 +16,30 @@ class MainActivity: FlutterActivity() {
     MapKitFactory.setLocale("ru_RU")
     MapKitFactory.setApiKey("7d7c37af-b634-485a-8642-40caa8f296b8") // API_KEY
     super.configureFlutterEngine(flutterEngine)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+      processMindboxIntent(intent)
+    }
+
+  override fun onNewIntent(intent: Intent) {
+    super.onNewIntent(intent)
+      processMindboxIntent(intent)
+    }
+
+  private fun processMindboxIntent(intent: Intent?) {
+    intent?.let {
+      // Проверяем, что интент - это пуш Mindbox
+      val uniqueKey = intent.getStringExtra("uniq_push_key")
+        if (uniqueKey != null) {
+        Mindbox.onPushClicked(this, it)
+          Mindbox.onNewIntent(it)
+          // Передача ссылки из пуша во Flutter
+          val link = Mindbox.getUrlFromPushIntent(intent) ?: ""
+          MindboxAndroidPlugin.pushClicked(link)
+
+        }
+    }
   }
 }
