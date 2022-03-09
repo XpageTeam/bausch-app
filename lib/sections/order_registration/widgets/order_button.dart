0461dc9c5c2388154d983b9d5b8efac9a7d79256
Danil Khandys <dreadlord74@yandex.ru>
2022-03-09 11:42:46 +0500

    подправил отображение текста

--- lib/sections/order_registration/widgets/order_button.dart
+++ lib/sections/order_registration/widgets/order_button.dart
@@ -49,7 +49,7 @@ class OrderButton extends StatelessWidget {
                             color: textColor,
                           ),
                         ),
-                      title,
+                      Flexible(child: title),
                     ],
                   ),
                 ),
