d99187251921ec89a1da823e458d5697928f739e
bonsoirelliot <privetdek16@gmail.com>
2022-03-30 14:33:59 +0500

    че

--- lib/sections/registration/code_screen.dart
+++ lib/sections/registration/code_screen.dart
@@ -27,65 +27,53 @@ class _CodeScreenState extends State<CodeScreen> {
         title: '',
         backgroundColor: Colors.transparent,
       ),
-      body: Align(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(
-            horizontal: StaticData.sidePadding,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(
-              bottom: 40,
-            ),
-            child: CodeForm(
-              wm: loginWM,
-            ),
-          ),
-        ),
+      body: CodeForm(
+        wm: loginWM,
       ),
-      floatingActionButton: StreamedStateBuilder<int>(
-        streamedState: loginWM.smsResendSeconds,
-        builder: (_, data) {
-          if (data > 0) {
-            return Container(
-              padding: const EdgeInsets.only(
-                bottom: 20,
-                //left: StaticData.sidePadding,
-                right: StaticData.sidePadding,
-              ),
-              width: MediaQuery.of(context).size.width,
-              //Повторная отправка через ${getTimerBySeconds(data)}',
-              child: RichText(
-                //textAlign: TextAlign.left,
-                text: TextSpan(
-                  style: AppStyles.p1,
-                  children: [
-                    const TextSpan(
-                      text: 'Повторная отправка через',
-                    ),
-                    TextSpan(
-                      text: ' ${getTimerBySeconds(data)}',
-                      style: AppStyles.p1.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          } else {
-            return TextButton(
-              child: const Text(
-                'Отправить новый код',
-                style: AppStyles.h2,
-              ),
-              style: TextButton.styleFrom(
-                padding: EdgeInsets.zero,
-              ),
-              onPressed: loginWM.resendSMSAction,
-            );
-          }
-        },
-      ),
+      // floatingActionButton: StreamedStateBuilder<int>(
+      //   streamedState: loginWM.smsResendSeconds,
+      //   builder: (_, data) {
+      //     if (data > 0) {
+      //       return Container(
+      //         padding: const EdgeInsets.only(
+      //           bottom: 20,
+      //           //left: StaticData.sidePadding,
+      //           right: StaticData.sidePadding,
+      //         ),
+      //         width: MediaQuery.of(context).size.width,
+      //         //Повторная отправка через ${getTimerBySeconds(data)}',
+      //         child: RichText(
+      //           //textAlign: TextAlign.left,
+      //           text: TextSpan(
+      //             style: AppStyles.p1,
+      //             children: [
+      //               const TextSpan(
+      //                 text: 'Повторная отправка через',
+      //               ),
+      //               TextSpan(
+      //                 text: ' ${getTimerBySeconds(data)}',
+      //                 style: AppStyles.p1.copyWith(
+      //                   fontWeight: FontWeight.bold,
+      //                 ),
+      //               ),
+      //             ],
+      //           ),
+      //         ),
+      //       );
+      //     } else {
+      //       return TextButton(
+      //         child: const Text(
+      //           'Отправить новый код',
+      //           style: AppStyles.h2,
+      //         ),
+      //         style: TextButton.styleFrom(
+      //           padding: EdgeInsets.zero,
+      //         ),
+      //         onPressed: loginWM.resendSMSAction,
+      //       );
+      //     }
+      //   },
+      // ),
       floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
     );
   }
--- lib/sections/registration/widgets/code_form/code_form.dart
+++ lib/sections/registration/widgets/code_form/code_form.dart
@@ -1,8 +1,10 @@
 import 'package:bausch/global/login/login_wm.dart';
 import 'package:bausch/packages/pin_code_fields/lib/pin_code_fields.dart';
+import 'package:bausch/static/static_data.dart';
 import 'package:bausch/theme/app_theme.dart';
 import 'package:bausch/theme/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:surf_mwwm/surf_mwwm.dart';
 
 class CodeForm extends StatelessWidget {
   final LoginWM wm;
@@ -14,52 +16,105 @@ class CodeForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    debugPrint(MediaQuery.of(context).size.height.toString());
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Сообщение с кодом\nбыло отправлено на\n${wm.phoneController.text}',
-          style: AppStyles.h1,
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height < 550 ? 20 : 100,
-        ),
-        SizedBox(
-          height: 100,
-          child: Center(
-            child: PinCodeTextField(
-              controller: wm.codeController,
-              animationType: AnimationType.none,
-              appContext: context,
-              length: 4,
-              autoFocus: true,
-              onChanged: (str) {},
-              enableActiveFill: true,
-              // focusNode: focusNode,
-              cursorColor: AppTheme.mineShaft,
-              keyboardType: TextInputType.phone,
-              onCompleted: (str) {
-                // focusNode.unfocus();
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(
+        StaticData.sidePadding,
+        20,
+        StaticData.sidePadding,
+        0,
+      ),
+      // padding: EdgeInsets.symmetric(horizontal: StaticData.sidePadding),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Сообщение с кодом\nбыло отправлено на\n${wm.phoneController.text}',
+            style: AppStyles.h1,
+          ),
+          // SizedBox(
+          //   height: MediaQuery.of(context).size.height < 450 ? 20 : 100,
+          // ),
+          SizedBox(
+            height: 100,
+            child: Center(
+              child: PinCodeTextField(
+                controller: wm.codeController,
+                animationType: AnimationType.none,
+                appContext: context,
+                length: 4,
+                autoFocus: true,
+                onChanged: (str) {},
+                enableActiveFill: true,
+                // focusNode: focusNode,
+                cursorColor: AppTheme.mineShaft,
+                keyboardType: TextInputType.phone,
+                onCompleted: (str) {
+                  // focusNode.unfocus();
 
-                wm.sendCodeAction();
-              },
-              pinTheme: PinTheme(
-                fieldHeight: 100,
-                fieldWidth: 63,
-                shape: PinCodeFieldShape.box,
-                activeColor: Colors.white,
-                borderWidth: 0,
-                selectedColor: Colors.white,
-                activeFillColor: Colors.white,
-                selectedFillColor: Colors.white,
-                inactiveFillColor: Colors.white,
-                inactiveColor: Colors.white,
+                  wm.sendCodeAction();
+                },
+                pinTheme: PinTheme(
+                  fieldHeight: 100,
+                  fieldWidth: 63,
+                  shape: PinCodeFieldShape.box,
+                  activeColor: Colors.white,
+                  borderWidth: 0,
+                  selectedColor: Colors.white,
+                  activeFillColor: Colors.white,
+                  selectedFillColor: Colors.white,
+                  inactiveFillColor: Colors.white,
+                  inactiveColor: Colors.white,
+                ),
               ),
             ),
           ),
-        ),
-      ],
+          StreamedStateBuilder<int>(
+            streamedState: wm.smsResendSeconds,
+            builder: (_, data) {
+              if (data > 0) {
+                return Container(
+                  padding: const EdgeInsets.only(
+                    bottom: 20,
+                    //left: StaticData.sidePadding,
+                    right: StaticData.sidePadding,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  //Повторная отправка через ${getTimerBySeconds(data)}',
+                  child: RichText(
+                    //textAlign: TextAlign.left,
+                    text: TextSpan(
+                      style: AppStyles.p1,
+                      children: [
+                        const TextSpan(
+                          text: 'Повторная отправка через',
+                        ),
+                        TextSpan(
+                          text: ' ${getTimerBySeconds(data)}',
+                          style: AppStyles.p1.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              } else {
+                return TextButton(
+                  child: const Text(
+                    'Отправить новый код',
+                    style: AppStyles.h2,
+                  ),
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                  ),
+                  onPressed: wm.resendSMSAction,
+                );
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
