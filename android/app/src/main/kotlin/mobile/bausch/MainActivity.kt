d31280883890bc7035b1a1d1abd77946887a4735
DaniilPavlov <cool.sharagin@mail.ru>
2022-07-08 15:06:19 +0400

    нативная обработка линков

--- android/app/src/main/kotlin/mobile/bausch/MainActivity.kt
+++ android/app/src/main/kotlin/mobile/bausch/MainActivity.kt
@@ -11,6 +11,8 @@ import io.flutter.embedding.engine.FlutterEngine
 
 
 class MainActivity: FlutterActivity() {
+  // private static final String CHANNEL = "https.bausch-lomb.in-progress.ru/channel";
+
   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
     MapKitFactory.setLocale("ru_RU")
     MapKitFactory.setApiKey("7d7c37af-b634-485a-8642-40caa8f296b8") // API_KEY
@@ -20,12 +22,52 @@ class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
       processMindboxIntent(intent)
+  //     GeneratedPluginRegistrant.registerWith(this);
+
+  // Uri data = intent.getData();
+
+  // new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+  //         new MethodChannel.MethodCallHandler() {
+  //           @Override
+  //           public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+  //             if (call.method.equals("initialLink")) {
+  //               if (startString != null) {
+  //                 result.success(startString);
+  //               }
+  //             }
+  //           }
+  //         });
+
+  // if (data != null) {
+  //   startString = data.toString();
+  // }
     }
 
   override fun onNewIntent(intent: Intent) {
     super.onNewIntent(intent)
       processMindboxIntent(intent)
+      //   if(intent.getAction() == android.content.Intent.ACTION_VIEW && linksReceiver != null) {
+      // linksReceiver.onReceive(this.getApplicationContext(), intent);
     }
+    }
+
+  //   private BroadcastReceiver createChangeReceiver(final EventChannel.EventSink events) {
+  //   return new BroadcastReceiver() {
+  //     @Override
+  //     public void onReceive(Context context, Intent intent) {
+  //       // NOTE: assuming intent.getAction() is Intent.ACTION_VIEW
+
+  //       String dataString = intent.getDataString();
+
+  //       if (dataString == null) {
+  //         events.error("UNAVAILABLE", "Link unavailable", null);
+  //       } else {
+  //         events.success(dataString);
+  //       }
+  //       ;
+  //     }
+  //   };
+  // }
 
   private fun processMindboxIntent(intent: Intent?) {
     intent?.let {
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -6,6 +6,9 @@ import mindbox_ios
 
 @UIApplicationMain
 @objc class AppDelegate: MindboxFlutterAppDelegate {
+    private var eventChannel: FlutterEventChannel?
+  
+  private let linkStreamHandler = LinkStreamHandler()
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -13,8 +16,53 @@ import mindbox_ios
     FirebaseApp.configure()
     YMKMapKit.setLocale("ru_RU")
     YMKMapKit.setApiKey("7d7c37af-b634-485a-8642-40caa8f296b8") // API_KEY
+
+    //  let controller = window.rootViewController as! FlutterViewController
+    // methodChannel = FlutterMethodChannel(name: "https.bausch-lomb.in-progress.ru/channel", binaryMessenger: controller)
+   
+    // methodChannel?.setMethodCallHandler({ (call: FlutterMethodCall, result: FlutterResult) in
+    //   guard call.method == "initialLink" else {
+    //     result(FlutterMethodNotImplemented)
+    //     return
+    //   }
+    // })
+    
     
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  //   override func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+  //   eventChannel?.setStreamHandler(linkStreamHandler)
+  //   return linkStreamHandler.handleLink(url.absoluteString)
+  // }
 }
+
+// class LinkStreamHandler:NSObject, FlutterStreamHandler {
+  
+//   var eventSink: FlutterEventSink?
+  
+//   // links will be added to this queue until the sink is ready to process them
+//   var queuedLinks = [String]()
+  
+//   func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+//     self.eventSink = events
+//     queuedLinks.forEach({ events($0) })
+//     queuedLinks.removeAll()
+//     return nil
+//   }
+  
+//   func onCancel(withArguments arguments: Any?) -> FlutterError? {
+//     self.eventSink = nil
+//     return nil
+//   }
+  
+//   func handleLink(_ link: String) -> Bool {
+//     guard let eventSink = eventSink else {
+//       queuedLinks.append(link)
+//       return false
+//     }
+//     eventSink(link)
+//     return true
+//   }
+// }
